f867b7dae2252d46cbc58d2300ca41f03a5112f3
flpeng00 <flpeng00@gmail.com>
2016-05-19 22:08:08 +0900

    lib파일 생성 dll 테스트용 GuitarSoriTest_Init 프로젝트 추가

--- GuitarSori/GuitarSori.cpp
+++ GuitarSori/GuitarSori.cpp
@@ -1,9 +1,9 @@
 // GuitarSori.cpp : DLL 응용 프로그램을 위해 내보낸 함수를 정의합니다.
 //
 
-#include "stdafx.h"
+#define GUITARSORI_API_EXPORTS
+#include <stdio.h>
 #include "GuitarSori.h"
-
 
 GuitarSori::GuitarSori()
 {
--- GuitarSori/GuitarSori.h
+++ GuitarSori/GuitarSori.h
@@ -1,12 +1,22 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C"
+#endif
+
+#ifdef GUITARSORI_API_EXPORTS
+#define MATHFUNCSDLL_API __declspec(dllexport) 
+#else
+#define MATHFUNCSDLL_API __declspec(dllimport) 
+#endif
+
 // PortAudio Library
 #include "../Portaudio/portaudio.h"
 #if PA_USE_ASIO
 #include "../Portaudio/pa_asio.h"
 #endif
 
-class GuitarSori
+class MATHFUNCSDLL_API GuitarSori
 {
 private:
 	int     numDevices, defaultDisplayed;
@@ -22,8 +32,7 @@ public:
 	void PrintSupportedStandardSampleRates(const PaStreamParameters *inputParameters, const PaStreamParameters *outputParameters);
 };
 
-double GuitarSori::standardSampleRates[14] = {
+double MATHFUNCSDLL_API GuitarSori::standardSampleRates[14] = {
 	8000.0, 9600.0, 11025.0, 12000.0, 16000.0, 22050.0, 24000.0, 32000.0,
 	44100.0, 48000.0, 88200.0, 96000.0, 192000.0, -1 
 };
-
